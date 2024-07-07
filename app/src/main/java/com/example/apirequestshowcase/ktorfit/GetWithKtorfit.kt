a2a169d598b3ce0cf3a69bc6be2d893e4fab9004
soleil.colza <soleil.colza@icloud.com>
2024-07-08 00:05:44 +0900

    replace: wildcard imports

--- app/src/main/java/com/example/apirequestshowcase/ktorfit/GetWithKtorfit.kt
+++ app/src/main/java/com/example/apirequestshowcase/ktorfit/GetWithKtorfit.kt
@@ -3,10 +3,10 @@ package com.example.apirequestshowcase.ktorfit
 import de.jensklingenberg.ktorfit.Ktorfit
 import de.jensklingenberg.ktorfit.http.GET
 import de.jensklingenberg.ktorfit.http.Path
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
